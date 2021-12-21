7a3e053fc926445de22d3168094871d9c2919512
Sherzod Yoqubov <92661003+Bahtiyorovich@users.noreply.github.com>
2021-12-21 09:33:15 +0500

    Add files via upload

---
+++ algoritm.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 16 23:05:42 2021
+
+@author: User
+"""
+
+#algoritmga misollar:
+
+# Foydalanuvchi kirirtgan ikki sonni qo'shish algoritmi python talqinida:
+
+# def summa(a,b):
+#     """Foydalanuvchi kiritgan ikki sonni qo'shish funksiyasi """
+#     return a+b
+
+
+
+# def cpr(a,b,c):
+#     """Berilgan uchta sondan kattasini topish funksiyasi"""
+#     if a>b or a>c:
+#         print(f"{a} eng katta son!")
+#     elif a<b or b>c:
+#         print(f"{b} eng katta son!")
+#     else:
+#          print(f"{c} eng katta son!")
+#     return a,b,c
+
+
+# faktarial hisoblash algoritmi python talqinida:
+
+# def fact(n):
+#     """Faktarial hisoblash funsiyasi"""
+#     faktarial=1
+#     i=1
+#     while i!=n+1:
+#         faktarial=faktarial*i
+#         i+=1
+        
+#     return faktarial
+    
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
